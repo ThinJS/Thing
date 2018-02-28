aaf2b6d53467806e8d85d85c3a136991aa77e879
macie <macie@users.noreply.github.com>
2018-02-28 20:57:41 +0100

    change Thing prototype to null

--- Thing.js
+++ Thing.js
@@ -1,11 +1,13 @@
 
-let Thing = {
-    clone: function(extension={}) {
-        let clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
-        return Object.assign(clone, extension);
-    },
-    extend: function(extension={}) {
-        let child = Object.create(this);
-        return Object.assign(child, extension);
-    }
+let Thing = Object.create(null);
+Thing.toString = () => "[object Thing]";
+
+Thing.clone = function(extension={}) {
+    let clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    return Object.assign(clone, extension);
+};
+
+Thing.extend = function(extension={}) {
+    let child = Object.create(this);
+    return Object.assign(child, extension);
 };
